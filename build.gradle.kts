41d5edf86c80cb3ea6e781f796df9e79f58c2610
johypark97 <johypark97@gmail.com>
2026-06-24 00:57:46 +0900

    chore: jjwt 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,9 @@ repositories {
 }
 
 dependencies {
+    implementation("io.jsonwebtoken:jjwt-api:0.13.0")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.13.0")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.13.0")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
